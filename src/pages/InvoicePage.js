b9e0d4c4588ab9a0e1ce2ee302720d123d73a396
leaviki90 <leaviki90@gmail.com>
2021-11-29 21:35:24 +0100

    some message

--- src/pages/InvoicePage.js
+++ src/pages/InvoicePage.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
-import ItemList from "../components/ItemList";
 import ViewInvoiceButtons from "../components/ViewInvoiceButtons";
 import Modal from "../components/Modal";
 import Form from "../components/Form";
